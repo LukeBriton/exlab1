7951cd99c6a321a3bf47f31585aeced8a7c8f356
Montagnard <928786745@qq.com>
2023-12-03 22:36:29 +0800

    Constructed a very rough static response

--- src/http.c
+++ src/http.c
@@ -1,7 +1,8 @@
 // http.c
 
 #include "http.h"
-
+#include <stdio.h>
+#include <string.h>
 // 请完成这个函数
 // 这个函数的参数是一个字符串，表示原始的请求，你可以用printf打印看看是啥
 // 这个函数的返回值是response，表示输出的响应，你需要按照格式来自行构造
@@ -10,9 +11,47 @@
 // 如果你不会写，可以试试最暴力地方法，先自己构造一个静态的response在浏览器里看看效果
 // 如果你实在不会，可以在answer文件夹里参考答案，然后自己独立实现出来
 char *http_process(char *request) {
-  static char response[1024]; // 如果1024不够用你还可以自己添加
-  *response = '\0';
-  
+	static char response[4096]; // 如果1024不够用你还可以自己添加->4096
+	//char response[8192];
+	char file_buffer[1024];
+	char line[512];//假设1行不会超过512个字符
+	*response = '\0';
+	printf("%s", request);//会爆烫烫烫
 
-  return response;
+	FILE* file;
+
+
+
+	file = fopen("src/htdocs/index.html", "r");//由于Visual Studio工程文件夹设在了exlab1，所以路径需要加上src/
+	//file = fopen("htdocs/index.html", "r");//不加的话正好作为404 Not Found的演示
+	if (file)
+	{
+		strcat(response, "HTTP/1.1 200 OK\r\n");//CR+LF Carriage Return Line Feed
+		strcat(response, "Server: ExLab1\r\n");
+		strcat(response, "Connection: keep-alive\r\n");
+		strcat(response, "Content-type: text/html\r\n");
+
+		strcat(response, "\r\n");//To End the Response Header
+		while (fgets(line, sizeof(line), file))
+			strcat(response, line);
+	}
+	else
+	{
+		file = fopen("src/htdocs/404.html", "r");
+		if (file)
+		{
+			strcat(response, "HTTP/1.1 404 Not Found\r\n");
+			strcat(response, "Server: ExLab1\r\n");
+			strcat(response, "Connection: close\r\n");
+			strcat(response, "Content-type: text/html\r\n");
+
+			strcat(response, "\r\n");
+			while (fgets(line, sizeof(line), file))
+				strcat(response, line);
+		}
+	}
+
+		//strcat(response, "404 Not Found\r\n");
+	fclose(file);
+	return response;
 }
